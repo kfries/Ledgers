763e2c92f66d81d44d9e51de31d83adff7b23b87
Kevin Fries <kfries@catalystsecure.com>
2013-01-27 12:24:39 -0700

    Finished Database Initialization

--- lib/database.rb
+++ lib/database.rb
@@ -1,2 +1,31 @@
+require 'mongo'
+
+class DBServerNotFound < StandardError
+   def initialize
+      super("ERROR: Server Not Found: Unable to find database server")
+   end
+end
+
 class Database
+   attr_reader :server
+   attr_reader :collection
+   attr_reader :dbname
+
+   def initialize(server=nil, collection=nil)
+      server = "localhost" if server.nil?
+      collection = "Ledgers" if collection.nil?
+
+      @server = server
+      @dbname = "Ledgers"
+      @collection = collection
+
+      begin
+         @client_obj = Mongo::MongoClient.new(@server)
+         @db_obj = @client_obj.db(@dbname)
+         @collection_obj = @db_obj[@collection]
+      rescue Mongo::ConnectionFailure
+         # raise DBServerNotFound, "Invalid Server Specified, Unable to connect to backend database"
+         raise DBServerNotFound
+      end
+   end
 end
--- spec/database_spec.rb
+++ spec/database_spec.rb
@@ -1,2 +1,102 @@
-class database_spec
+$: << "../lib"
+
+require 'spec_helper'
+require 'database'
+
+describe Database do
+   describe "#new" do
+      describe "with no parameters" do
+         before do
+            @db = Database.new
+         end
+
+         it "Server should be localhost" do
+            @db.server.should == "localhost"
+         end
+
+         it "Collection should be Ledgers" do
+            @db.collection.should == "Ledgers"
+         end
+
+         it "Databse Name should be Ledgers" do
+            @db.dbname.should == "Ledgers"
+         end
+      end
+
+      describe "with a server and no collection passed" do
+         before do
+            @db = Database.new("kfries-laptop")
+         end
+
+         it "Server should be kfries-laptop" do
+            @db.server.should == "kfries-laptop"
+         end
+
+         it "Collection should be Ledgers" do
+            @db.collection.should == "Ledgers"
+         end
+
+         it "Databse Name should be Ledgers" do
+            @db.dbname.should == "Ledgers"
+         end
+      end
+
+      describe "with a collection and no server passed" do
+         before do
+            @db = Database.new(nil, "testing")
+         end
+
+         it "Server should be localhost" do
+            @db.server.should == "localhost"
+         end
+
+         it "Collection should be testing" do
+            @db.collection.should == "testing"
+         end
+
+         it "Databse Name should be Ledgers" do
+            @db.dbname.should == "Ledgers"
+         end
+      end
+
+      describe "with both a server and collection passed" do
+         before do
+            @db = Database.new("kfries-laptop", "testing")
+         end
+
+         it "Server should be kfries-laptop" do
+            @db.server.should == "kfries-laptop"
+         end
+
+         it "Collection should be testing" do
+            @db.collection.should == "testing"
+         end
+
+         it "Databse Name should be Ledgers" do
+            @db.dbname.should == "Ledgers"
+         end
+      end
+
+      describe "when non existant servername exists" do
+         it "Should fail with ServerNotFound error" do
+            lambda {Database.new("ThisShouldFail.com")}.should raise_error(DBServerNotFound)
+         end
+      end
+   end
+
+   describe "#getLedgerList" do
+      pending
+   end
+
+   describe "#getLedger" do
+      pending
+   end
+
+   describe "#addLedger" do
+      pending
+   end
+
+   describe "#updateLedger" do
+      pending
+   end
 end
